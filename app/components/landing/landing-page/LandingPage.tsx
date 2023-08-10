04389b7f228e51716fc49bb8d9404ad7f64e0d8b
Roko <rokoponjarac2@gmail.com>
2023-08-10 18:14:51 +0200

    create landing page

--- app/components/landing/landing-page/LandingPage.tsx
+++ app/components/landing/landing-page/LandingPage.tsx
@@ -1,10 +1,12 @@
-import BudgetResults from "../budget/budget-results/BudgetResults";
+import BudgetResults from "../budget/budget-results/BudgetResultsSection";
+import BudgetTrackerSection from "../budget/budget-tracker/BudgetTrackerSection";
 import styles from "./LandingPage.module.scss";
 
 const LandingPage = () => {
   return (
     <div className={styles.budget_container}>
       <BudgetResults />
+      <BudgetTrackerSection />
     </div>
   );
 };
